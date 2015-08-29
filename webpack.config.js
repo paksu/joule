43ac2717de39cf586a9c289c534988c022bb6ab4
Joona Pääkkönen <joona@giosg.com>
2015-08-29 10:28:34 +0300

    Configured hot reload for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,32 @@ var BundleTracker = require('webpack-bundle-tracker');
 module.exports = {
   context: __dirname,
 
-  entry: './assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+  entry: [
+    // for hot reload
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+     // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+    './assets/js/index',
+  ],
 
   output: {
       path: path.resolve('./assets/bundles/'),
       filename: "[name]-[hash].js",
+      // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
+      publicPath: 'http://localhost:3000/assets/bundles/',
   },
 
+
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(), // don't reload if there is an error
     new BundleTracker({filename: './webpack-stats.json'}),
   ],
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}, // to transform JSX into JS
+      // we pass the output from babel loader to react-hot loader
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'], },
     ],
   },
 

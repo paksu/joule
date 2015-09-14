0d535c9eed3ab030a2a024de33de69290252bb6a
Joona Pääkkönen <joona@giosg.com>
2015-09-14 22:38:07 +0300

    Macro chart

--- assets/js/components/Overview.jsx
+++ assets/js/components/Overview.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import {Line as LineChart} from 'react-chartjs';
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
+import {Pie as PieChart} from 'react-chartjs';
+
+var FAT = {
+    value: 300,
+    color:'#F7464A',
+    highlight: '#FF5A5E',
+    label: 'Fat'
+}
+var PROTEIN = {
+    value: 50,
+    color: '#46BFBD',
+    highlight: '#5AD3D1',
+    label: 'Protein'
+}
+var CARBS = {
+    value: 100,
+    color: '#FDB45C',
+    highlight: '#FFC870',
+    label: 'Carbs'
 };
+var data = [FAT, PROTEIN, CARBS];
 var chartOptions = {};
 
 export default class Overview extends React.Component {
@@ -32,9 +27,25 @@ export default class Overview extends React.Component {
   render() {
     const { foods } = this.props;
     if(!foods || !foods.length) return (<span/>);
+
+    const flatten = list => list.reduce(
+        (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+    );
+
+    var componentSums = {};
+    flatten(foods.map(food => food.components)).forEach(component => {
+      let value = parseFloat(component.value);
+      componentSums[component.code] = componentSums[component.code] ? componentSums[component.code] + value : value;
+    });
+
+
+    FAT.value = componentSums.fat;
+    PROTEIN.value = componentSums.prot;
+    CARBS.value = componentSums.choavl;
+
     return (
       <div>
-        return <LineChart data={data} options={chartOptions} width="600" height="250"/>
+        <PieChart data={data} options={chartOptions} width="100" height="100"/>
       </div>
     );
   }
